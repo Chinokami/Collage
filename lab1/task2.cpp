7b4c25d3da79ade6b1115434f42acaa74dffb9e0
Chino <paypalnawaki@gmail.com>
2026-02-04 14:31:47 +0200

    Add task2.cpp for finding largest multiple of k
    Implement a program to find the largest positive multiple of k before the first negative number in an array.

---
+++ lab1/task2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  int k;
+  cin >> k;
+  
+  int* arr = new int[n];
+  
+  srand(time(NULL));
+  for (int i = 0; i < n; i++) {
+    arr[i] = rand() % 201 - 100;
+    cout << arr[i] << " ";
+  }
+  
+  int result = 0;
+  
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < 0) {
+      break;
+    }
+    
+    if (arr[i] % k == 0 && arr[i] > result) {
+      result = arr[i];
+    }
+  }
+  
+  if (result > 0)
+    cout << endl << result << endl;
+  else
+    cout << endl << "Not found";
+  
+  delete[] arr;
+  return 0;
+}
