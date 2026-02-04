f9a9266d02ae1b56e838a4aa4f42ad9bb3deda4c
Chino <paypalnawaki@gmail.com>
2026-02-04 22:13:05 +0200

    Implement matrix generation and evaluation program

---
+++ lab1/task5_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  if (n < 2)
+    return 1;
+    
+  int** matrix = new int*[n];
+  
+  for (int i = 0; i < n; i++)
+    matrix[i] = new int[n];
+  
+  srand(time(NULL));
+  cout << "Matrix:" << endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      matrix[i][j] = rand() % 11;
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
+  
+  int* result = new int[n];
+  
+  for (int i = 0; i < n; i++) {
+    int min = 0;
+    for (int j = 0; j < n; j++) {
+      if (matrix[i][j] >= min)
+        min = matrix[i][j];
+      else {
+        min = -1;
+        break;
+      }
+    }
+    
+    int max = 10;
+    for (int j = 0; j < n; j++) {
+      if (matrix[i][j] <= max)
+        max = matrix[i][j];
+      else {
+        max = -1;
+        break;
+      }
+    }
+    
+    if (min != -1 || max != -1)
+      result[i] = 1;
+    else
+      result[i] = 0;
+  }
+  
+  cout << "Result:" << endl;
+  for (int i = 0; i < n; i++)
+    cout << result[i] << " ";
+
+  delete[] matrix;
+  delete[] result;
+  
+  return 0;
+}
