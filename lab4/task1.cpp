702a93a5bff605a30797769fdd104aeef3510c91
Chino <paypalnawaki@gmail.com>
2026-04-29 15:13:15 +0300

    Add task1.cpp to calculate sphere properties

---
+++ lab4/task1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+using namespace std;
+
+int main() {
+  int n = 15;
+  double a, b, c;
+  
+  srand(time(NULL));
+  for (int i = 1; i <= n; i++) {
+    a = rand() % 11 - 5;
+    b = rand() % 11 - 5;
+    c = rand() % 11 - 5;
+    
+    
+    double R = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
+    
+    if (R == 3) {
+      double S = 4 * pow(R, 2);
+      double V = (4 / 3) * pow(R, 3);
+      
+      cout << "Number: " << i << endl;
+      cout << a << "² + " << b << "² + " << c << "² = " << R << "²" << endl;
+      cout << "S = " << S << "π" << endl;
+      cout << "V = " << V << "π" << endl;
+    }
+  }
+  
+  return 0;
+}
